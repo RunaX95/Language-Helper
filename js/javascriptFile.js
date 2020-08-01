0b1777dd28db7ac4886f44fd040f13b6e4fa8000
iamrunax <runarunax@myemail.com>
2020-08-01 11:36:29 +0200

    Added the full funtion too all the cards in js

--- js/javascriptFile.js
+++ js/javascriptFile.js
@@ -1,20 +1,5 @@
 "use strict"
 
-/*var firstButton = document.getElementsByClassName('card-button')[0];
-var firstAnswer = document.getElementsByClassName('card-answer')[0];
-
-/*firstButton.addEventListener('click', function(event){
-    
-    if(firstButton.innerHTML == 'Show'){
-        firstAnswer.style.display = 'Block';
-        firstButton.innerHTML = 'Hide';
-    }else if(firstButton.innerHTML == 'Hide'){
-        firstAnswer.style.display = 'None';
-        firstButton.innerHTML = 'Show';
-    }
-    
-})*/
-
 var cards = document.getElementsByClassName('card');
 for(var cardIndex in cards) {
     var button = cards[cardIndex].querySelector('.card-button');
